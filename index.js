46f0652db6a19b00f840708962bea382b279eb98
barsu5489 <ebarsulai@gmail.com>
2022-11-29 20:02:03 +0300

    array methods

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+// const result = records.find(records)
+// function superbowlWin(record)
+// {
+//     if (record == "W")
+//     {
+//         return result.year
+//     }
+//     else if (record != "W" && record != "L")
+//     {
+//         return undefined
+//     }
+// }
+function superbowlWin(record){
+for(let item of record)
+{
+    if (item.result == "W")
+    {
+        return item.year;
+    }
+}
+}
